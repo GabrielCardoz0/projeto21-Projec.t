e83adb1edf2d5d3aeab5e6633188403650aeb5f1
Gabriel Cardozo <gabriel.scardozo7@gmail.com>
2023-03-29 21:34:29 -0300

    feat: install test dependencies and create helpers cleandb and generateValidToken

---
+++ back-end/tests/helpers.ts
@@ -0,0 +1,23 @@
+import prisma from "../src/config/database";
+import jwt from "jsonwebtoken";
+
+type User = {
+  id: number,
+  email: string,
+  name: string
+};
+
+export async function cleanDb() {
+  await prisma.task.deleteMany({});
+  await prisma.sprint.deleteMany({});
+  await prisma.notes.deleteMany({});
+  await prisma.project.deleteMany({});
+  await prisma.userProject.deleteMany({});
+  await prisma.user.deleteMany({});
+}
+
+export async function generateValidToken(user: User) {
+  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
+
+  return token;
+}
